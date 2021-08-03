916da73718814dd5275ae7b0d09dc26f48c0e0bc
Maram-Ankir <maramankir5@gmail.com>
2021-08-03 06:36:59 +0300

    code challenge 7

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -12,91 +12,121 @@
 #         pass
 
 
-# class Node:
-#     def __init__(self, value):
-#         self.value = value
-#         self.next = None
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
-# class LinkedList:
-#     inestance=[]
+class LinkedList:
+    inestance=[]
 
-#     def __init__(self):
-#         self.head = None
+    def __init__(self):
+        self.head = None
 
-#     def insert(self, value):
+    def insert(self, value):
 
-#         node = Node(value)
-#         if self.head == None:
-#             self.head = node
-#             return self.head.value
-#         else:
-#             current = self.head
-#             self.head = node
-#             self.head.next = current
-#             return self.head.value
-
-
-#     def __str__(self):
-
-#         if self.head == None:
-#             return 'NULL'
-#         else :
-#             values=''
-#             temporary_value=self.head
-#             while temporary_value:
-#                 values+=f'{temporary_value.value}'  + '-> '
-#                 temporary_value=temporary_value.next
-#             values=values +'NULL'
-#             return f'{values}'
-
-#     def append(self,value):
-#         current=self.head
-#         if self.head ==None:
-#             self.head=Node(value)
-#             return self.head.value
-#         else:
-#             while current.next:
-#                 current=current.next
-#             current.next=Node(value)
-#             return current.next
+        node = Node(value)
+        if self.head == None:
+            self.head = node
+            return self.head.value
+        else:
+            current = self.head
+            self.head = node
+            self.head.next = current
+            return self.head.value
 
 
-# def insert_before(self,val,new_val):
-#             if self.head ==None:
-#                 self.head=Node(val)
-#             if self.head.value == val:
-#                 self.insert(new_val)
-#             else:
-#                 try:
-#                     current=self.head
-#                     while current.next:
-#                         if  current.next.value== val:
-#                             saved_current_val=current.next
-#                             current.next=Node(new_val)
-#                             current.next.next=saved_current_val
-#                             return current.next
-#                         current=current.next
-#                 except:
-#                     print(f'{val} is not in linked list')
+    def __str__(self):
+
+        if self.head == None:
+            return 'NULL'
+        else :
+            values=''
+            temporary_value=self.head
+            while temporary_value:
+                values+=f'{temporary_value.value}'  + '-> '
+                temporary_value=temporary_value.next
+            values=values +'NULL'
+            return f'{values}'
+
+    def append(self,value):
+        current=self.head
+        if self.head ==None:
+            self.head=Node(value)
+            return self.head.value
+        else:
+            while current.next:
+                current=current.next
+            current.next=Node(value)
+            return current.next
 
 
-# def insert_after(self,val,new_val):
-#         try:
-#             current=self.head
-#             if self.head ==None:
-#                 self.head=Node(val)
-#                 return self.head
-#             else:
-#                 while current:
-#                     if  current.value== val:
-#                         saved_current_val=current.next
-#                         current.next=Node(new_val)
-#                         current.next.next=saved_current_val
-#                         return current.next
-#                     current=current.next
-#         except:
-#             print('Error')
+def insert_before(self,val,new_val):
+            if self.head ==None:
+                self.head=Node(val)
+            if self.head.value == val:
+                self.insert(new_val)
+            else:
+                try:
+                    current=self.head
+                    while current.next:
+                        if  current.next.value== val:
+                            saved_current_val=current.next
+                            current.next=Node(new_val)
+                            current.next.next=saved_current_val
+                            return current.next
+                        current=current.next
+                except:
+                    print(f'{val} is not in linked list')
+
+
+def insert_after(self,val,new_val):
+        try:
+            current=self.head
+            if self.head ==None:
+                self.head=Node(val)
+                return self.head
+            else:
+                while current:
+                    if  current.value== val:
+                        saved_current_val=current.next
+                        current.next=Node(new_val)
+                        current.next.next=saved_current_val
+                        return current.next
+                    current=current.next
+        except:
+            print('Error')
+
+def kthFromEnd(self,k):
+        current=self.head
+        length=1
+        while current.next:
+            length+=1
+            current=current.next
+        current=self.head
+        if k>= length:
+            return 'Error! index out of range'
+        elif k<0:
+            return "Error! k can't be negative number"
+        else:
+            count =length-k-1
+            for i in range(length):
+                    if i == count:
+                        return current.value
+                    current =current.next
+
+def kth(self,j):
+        current=self.head
+        val=[]
+        while current:
+            val+=[current.value]
+            current=current.next
+        val=val[::-1]
+
+        for i in range(len(val)):
+            if i==j:
+                return val[j]
 
 
 # ll1=LinkedList()
