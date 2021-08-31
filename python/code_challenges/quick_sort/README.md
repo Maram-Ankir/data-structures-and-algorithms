5fe0c0b0c6a4050ff9f2cabc85ebb14be1548965
Maram-Ankir <maramankir5@gmail.com>
2021-09-01 00:25:23 +0300

    finish challeng 28

---
+++ python/code_challenges/quick_sort/README.md
@@ -0,0 +1,15 @@
+# Challenge Summary
+
+Write a function that sort an array ascendingly using quick sort.
+
+## Whiteboard Process
+
+![quick sort](ch28.jpg)
+
+## Approach & Efficiency
+
+time O(n2)
+space O(1)
+
+## Solution
+ quick sort function that take an array and return the array with ascendining order
--- python/code_challenges/quick_sort/tests/test_quick_sort.py
+++ python/code_challenges/quick_sort/tests/test_quick_sort.py
@@ -1,5 +1,19 @@
 from quick_sort import __version__
+from quick_sort.quick_sort import quickSort
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_success_sort():
+    actual=quickSort([75,15,6,2,8,3],0,5)
+    assert actual==[2,3,6,8,15,75]
+
+def test_Reverse_sort():
+    actual=quickSort([20,18,12,8,5,-2],0,5)
+    assert actual==[-2,5,8,12,18,20]
+
+
+def test_nearly_sorted_sort():
+    actual=quickSort([2,3,5,7,13,11],0,5)
+    assert actual==[2,3,5,7,11,13]
